0e8d887897266996ab20bb78fb445118f4e80796
unknown <lucioenricci10@gmail.com>
2023-01-20 12:00:57 +0100

    cargando segunda entrega a github

--- src/components/itemDetail/ItemDetail.js
+++ src/components/itemDetail/ItemDetail.js
@@ -2,13 +2,11 @@
                     Importaciones
 ##########################################*/
 //Modulos
-import { Link } from 'react-router-dom';
 
 //Estilos
 import './ItemDetail.css';
 
 //Componentes
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ItemCount from '../itemCount/ItemCount.js';
 
@@ -18,21 +16,21 @@ import ItemCount from '../itemCount/ItemCount.js';
                     Logica
 ##########################################*/
 const ItemDetail = (props) => {
-    const {title, category, description, price} = props.data; 
+  const { title, category, description, price } = props.data;
 
-    return (
-        <Card className='card'>
-        <Card.Header as="h5">{category}</Card.Header>
-        <Card.Body>
-          <Card.Title>{title} || price = ${price}</Card.Title>
-          <Card.Text>
+  return (
+    <Card className='card'>
+      <Card.Header as="h5">{category}</Card.Header>
+      <Card.Body>
+        <Card.Title>{title} || price = ${price}</Card.Title>
+        <Card.Text>
           {description}
-          </Card.Text>
-          <Button variant="primary">Add to cart</Button>
-        </Card.Body>
-        <ItemCount stock="10"/>
-      </Card>
-    )
+        </Card.Text>
+        <ItemCount stock="10" />
+      </Card.Body>
+      <h2>Mas Productos</h2>
+    </Card>
+  )
 };
 
 /*########################################
